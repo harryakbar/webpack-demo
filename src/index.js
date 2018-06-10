5a42f71d0970c9cd71fb935911c994a1fa40a2db
Harry Akbar Ali Munir <harry.munir@happyfresh.com>
2018-06-10 21:05:25 +0700

    Loading Image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import MyImage from './image.jpg';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    var myIcon = new Image();
+    myIcon.src = MyImage;
+
+    element.appendChild(myIcon);
+
     return element;
   }
   
