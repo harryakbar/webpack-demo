07fe959385ffde7fdc52a0c2060d281e9c854ca7
Harry Akbar Ali Munir <harry.munir@happyfresh.com>
2018-06-10 23:38:21 +0700

    Loading font

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
-import MyImage from './image.jpg';
 import './style.css';
+import MyImage from './image.jpg';
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.createElement('p');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
     var myIcon = new Image();
     myIcon.src = MyImage;
-
     element.appendChild(myIcon);
-    element.classList.add('hello');
 
     return element;
   }
